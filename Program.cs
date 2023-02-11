bd2f7fa1a10c104aeb4ccfc8b0ae0afcb21e6113
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-02-11 12:53:28 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+ThreadPool.SetMaxThreads(7, 7);
+
+
+Parallel.For(0, 100, (index) =>
+{
+    Thread.CurrentThread.Name = index.ToString();
+    MyApi((MyHTTPMethod)Math.Floor((decimal)new Random().Next(3)), index);
+});
+
+string MyApi(MyHTTPMethod method, int data)
+{
+    Console.WriteLine($"ApiCall name: {Thread.CurrentThread.Name} - Method: {method.ToString()} - Started");
+
+    Thread.Sleep(2000);
+
+    Console.WriteLine($"ApiCall name: {Thread.CurrentThread.Name} - Method: {method.ToString()} - End");
+
+    return "200";
+}
+
+enum MyHTTPMethod
+{
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
